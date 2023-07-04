0ecdcc8eda4783e0a30740b32bd682bfb034ca73
resh1995maas <129730758+resh1995maas@users.noreply.github.com>
2023-07-04 13:25:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const fs=require('fs');
+
+const app=new express();
+const port=3000;
+const PORT=process.env.PORT || 3000;
+
+//Middleware
+
+app.use(bodyParser.json());
+
+
+
+
+//Read hospital data from json file
+
+const readHospitalsData = ()=>{
+    const rawData=fs.readFileSync('hospitals.json');
+    return JSON.parse(rawData);
+};
+
+
+
+
+
+
+//Write Hosptal data to json file
+
+const writeHospitalsData = (data)=>{
+    fs.writeFileSync('hospitals.json',JSON.stringify(data,null,2));
+};
+
+
+
+
+//GET all hospitals
+
+app.get('/hospitals',(req,res)=>{
+    const hospitals=readHospitalsData();
+    res.json(hospitals);
+});
+
+
+
+//GET a specific hospital by index
+
+app.get('/hospitals/:index',(req,res)=>{
+    const hospitals=readHospitalsData();
+    const index=req.params.index;
+    if (index < hospitals.length) {
+
+        res.json(hospitals[index]);
+        
+    } else {
+
+        res.status(404).send('Hosppital not found.');
+        
+    }
+});
+
+
+
+
+//POST a new hospital
+
+app.post('/hospitals', (req, res) => {
+    const hospitals = readHospitalsData();
+    const newHospital = req.body;
+    hospitals.push(newHospital);
+    writeHospitalsData(hospitals);
+    res.send('Hospital added successfully.');
+  });
+
+  // PUT (update) a hospital by index
+app.put('/hospitals/:index', (req, res) => {
+    const hospitals = readHospitalsData();
+    const index = req.params.index;
+    if (index < hospitals.length) {
+      hospitals[index] = req.body;
+      writeHospitalsData(hospitals);
+      res.send('Hospital updated successfully.');
+    } else {
+      res.status(404).send('Hospital not found.');
+    }
+  });
+  
+
+  // DELETE a hospital by index
+app.delete('/hospitals/:index', (req, res) => {
+    const hospitals = readHospitalsData();
+    const index = req.params.index;
+    if (index < hospitals.length) {
+      hospitals.splice(index, 1);
+      writeHospitalsData(hospitals);
+      res.send('Hospital deleted successfully.');
+    } else {
+      res.status(404).send('Hospital not found.');
+    }
+  });
+
+  // Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
